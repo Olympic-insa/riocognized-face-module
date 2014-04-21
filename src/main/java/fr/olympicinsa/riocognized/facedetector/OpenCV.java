32ce70eed3faecbd692d576dd7f880e0af071256
imanerai <imane0310@gmail.com>
2014-04-21 23:45:08 +0200

    conversion d'images

--- src/main/java/fr/olympicinsa/riocognized/facedetector/OpenCV.java
+++ src/main/java/fr/olympicinsa/riocognized/facedetector/OpenCV.java
@@ -83,62 +83,7 @@ public class OpenCV {
         return INSTANCE;
     }
 
-    /**
-     * Converts/writes a Mat into a BufferedImage.
-     *
-     * @param matrix Mat of type CV_8UC3 or CV_8UC1
-     * @return BufferedImage of type TYPE_3BYTE_BGR or TYPE_BYTE_GRAY
-     */
-    public BufferedImage matToBufferedImage(Mat matrix) {
-        int cols = matrix.cols();
-        int rows = matrix.rows();
-        int elemSize = (int) matrix.elemSize();
-        byte[] data = new byte[cols * rows * elemSize];
-        int type;
 
-        matrix.get(0, 0, data);
-
-        switch (matrix.channels()) {
-            case 1:
-                type = BufferedImage.TYPE_BYTE_GRAY;
-                break;
-
-            case 3:
-                type = BufferedImage.TYPE_3BYTE_BGR;
-
-                // bgr to rgb
-                byte b;
-                for (int i = 0; i < data.length; i = i + 3) {
-                    b = data[i];
-                    data[i] = data[i + 2];
-                    data[i + 2] = b;
-                }
-                break;
-
-            default:
-                return null;
-        }
-
-        BufferedImage image = new BufferedImage(cols, rows, type);
-        image.getRaster().setDataElements(0, 0, cols, rows, data);
-
-        return image;
-    }
-
-    /**
-     * Converts/writes a BufferedImage into a Mat.
-     *
-     * @param image BufferedImage of type TYPE_3BYTE_BGR or TYPE_BYTE_GRAY
-     * @return matrix Mat of type CV_8UC3 or CV_8UC1
-     */
-    public Mat BufferedImagetoMat(BufferedImage image) {
-        int rows = image.getWidth();
-        int cols = image.getHeight();
-        byte[] data = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-        Mat mat = new Mat(cols, rows, CvType.CV_8UC3);
-        mat.put(0, 0, data);
-        return mat;
-    }
 
     /**
      * libraryPath getter
