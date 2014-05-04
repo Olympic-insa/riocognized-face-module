8792e225fbf1696f711f93f4e6f5c22fcd80db7f
alexisduque <alexisd61@gmail.com>
2014-05-04 19:05:50 +0200

    refactoring main class for testing purpose

--- src/main/java/fr/olympicinsa/riocognized/facedetector/Riocognized.java
+++ src/main/java/fr/olympicinsa/riocognized/facedetector/Riocognized.java
@@ -4,6 +4,7 @@ import fr.olympicinsa.riocognized.facedetector.detection.FaceDetector;
 import fr.olympicinsa.riocognized.facedetector.tools.ImageConvertor;
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
 import fr.olympicinsa.riocognized.facedetector.db.FaceDBReader;
+import fr.olympicinsa.riocognized.facedetector.exception.FaceDBException;
 import fr.olympicinsa.riocognized.facedetector.recognition.RioRecognizer;
 import java.io.File;
 import java.io.IOException;
@@ -20,8 +21,9 @@ import org.opencv.highgui.Highgui;
 public class Riocognized {
 
     public static Logger log = Logger.getLogger(Riocognized.class);
-    public static String IMAGE_TO_RECOGNIZE = "/opt/openCV/teddy.jpg";
-    public static String DIR_TO_RECOGNIZE = "/opt/openCV";
+    public static String IMAGE_TO_RECOGNIZE = "/opt/openCV/testImage/teddy.jpg";
+    public static String DIR_TO_RECOGNIZE = "/opt/openCV/testImage";
+    public static String DIR_TO_DB = "/opt/openCV/lfwDB/faces.csv";
 
     public static void main(String[] args) {
 
@@ -31,52 +33,59 @@ public class Riocognized {
         DateFormat dateFormat = new SimpleDateFormat("hhmmss-dd-MM-yy");
         Date date = new Date();
         String dateString = dateFormat.format(date);
-
+        String db = (args.length > 1) ? args[1] : DIR_TO_DB;
         String athletePath;
         FaceDBReader faceDB;
 
         String path = "/opt/openCV/test.yml";
         //Load faceDB
-        faceDB = new FaceDBReader("/opt/openCV/athleteDB/faces.csv");
-        //Create  Recognizor
-        log.info("Create Recognizer");
-        RioRecognizer recognizor = new RioRecognizer(faceDB, path);
-        //Store faces
-        log.info("Read CSV and load images");
-        recognizor.init();
-        log.info("Train Recognizer");
-        recognizor.train();
-        log.info("Save Recognizer");
-        recognizor.save();
-
-        log.info("Initialize Riocognized FaceDetector");
-
-        File repertoire = new File(DIR_TO_RECOGNIZE);
-        String fileOutput = null;
-        if (repertoire.isDirectory()) {
-            File[] list = repertoire.listFiles();
-            for (File file : list) {
-                if (file.getName().matches("(.*)jpg")) {
-                    fileOutput = file.getAbsolutePath();
-                    String fileName = FilenameUtils.removeExtension(file.getName());
-                    recognizeTest(fileOutput, fileName, recognizor);
-                }
-            }
-        }
-        String imageParam = (args.length > 0) ? args[0] : IMAGE_TO_RECOGNIZE;
-        File f = new File(imageParam);
         try {
-            if (!f.exists()) {
-                throw new IOException();
+            faceDB = new FaceDBReader(db);
+
+            //Create  Recognizor
+            log.info("Create Recognizer");
+            RioRecognizer recognizor = new RioRecognizer(faceDB, path);
+            //Store faces
+            log.info("Read CSV and load images");
+            recognizor.init();
+            log.info("Train Recognizer");
+            recognizor.train();
+            log.info("Save Recognizer");
+            recognizor.save();
+
+            log.info("Initialize Riocognized FaceDetector");
+            String param = (args.length > 0) ? args[0] : IMAGE_TO_RECOGNIZE;
+            File repertoire = new File(param);
+            String fileOutput = null;
+            if (repertoire.isDirectory()) {
+                File[] list = repertoire.listFiles();
+                for (File file : list) {
+                    if (file.getName().matches("(.*)jpg")) {
+                        fileOutput = file.getAbsolutePath();
+                        String fileName = FilenameUtils.removeExtension(file.getName());
+                        recognizeTest(fileOutput, fileName, recognizor);
+                    }
+                }
+            } else {
+                String imageParam = param;
+                File f = repertoire;
+                try {
+                    if (!f.exists()) {
+                        throw new IOException();
+                    }
+                    athletePath = imageParam;
+
+                } catch (IOException e) {
+                    log.error("File not found");
+                    exit(0);
+                    athletePath = Riocognized.class.getResource("/image.jpg").getPath();
+                }
+                recognizeTest(imageParam, FilenameUtils.removeExtension(f.getName()), recognizor);
             }
-            athletePath = imageParam;
-            
-        } catch (IOException e) {
-            log.error("File not found");
+        } catch (FaceDBException e) {
+            log.error("Can't read/create faceDB csv");
             exit(0);
-            athletePath = Riocognized.class.getResource("/image.jpg").getPath();
         }
-        recognizeTest(imageParam, FilenameUtils.removeExtension(f.getName()),recognizor);
     }
 
     public static void recognizeTest(String fileOutput, String filename, RioRecognizer recognizor) {
@@ -95,7 +104,7 @@ public class Riocognized {
                 if (faceDetector.getFacesDetected() > 0) {
                     Highgui.imwrite(filename + "_croped.jpg", crop);
                     IplImage face = ImageConvertor.matToIplImage(crop);
-                    for (int i = 0; i < 5; i++) {
+                    for (int i = 0; i < 4; i++) {
                         int athlete = recognizor.predictedLabel(face);
                         System.out.println("\nAthlete recognized : " + athlete + "\n");
                         recognizor.changeRecognizer(i);
